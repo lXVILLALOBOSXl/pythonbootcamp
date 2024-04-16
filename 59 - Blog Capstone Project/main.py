7653062e5e1f0a55132e51df827e6af25b31a86b
Luis Villalobos <ivillalobosrivera@yahoo.com>
2024-04-16 14:53:25 -0600

    day 59

---
+++ 59 - Blog Capstone Project/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    response = requests.get(f"https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    result = response.json()
+    return render_template("index.html", posts=result)
+
+@app.route("/post/<id>")
+def get_post(id):
+    id = int(id)
+    response = requests.get(f"https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    result = response.json()
+    target_post = next((item for item in result if item["id"] == id), None)
+    id2 = id
+    return render_template('blog-details.html', title=target_post['title'], body=target_post['body'], id=id2)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
