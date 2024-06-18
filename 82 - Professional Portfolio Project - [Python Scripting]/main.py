f7ab2a9d7450a5050e35074b30596b1686ae35c3
Luis Villalobos <ivillalobosrivera@yahoo.com>
2024-06-18 09:37:08 -0600

    day 82

---
+++ 82 - Professional Portfolio Project - [Python Scripting]/main.py
@@ -0,0 +1,30 @@
+from dict import morse
+
+def encode(message:str):
+    code = []
+    for letter in message:
+        code.append(morse[letter])
+    return code
+
+def main():
+    message = input('Write a message: ')
+    message = message.lower()
+    code = encode(message)
+    print(f'message in morse is: {code}')
+    pass
+
+# if __name__ == '__main__':
+#     main()
+
+# from dict import morse
+
+# def encode(message: str) -> list:
+#     return [morse[letter] for letter in message]
+
+# def main():
+#     message = input('Write a message: ').lower()
+#     code = encode(message)
+#     print(f'Message in Morse code is: {code}')
+
+# if __name__ == '__main__':
+#     main()
